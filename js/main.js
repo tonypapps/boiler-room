893942d74212841f45177a44fbe54566bfa52532
Anthony Pappalardo <anpa7322@colorado.edu>
2018-04-03 15:37:49 -0600

    countdown timer

--- js/main.js
+++ js/main.js
@@ -49,3 +49,35 @@ function previousImage(){
 
 }
 slideshow();
+
+// ===========Countdown Clock========
+function cdtd() {
+  var date = new Date('May 15, 2018 00:00:01');
+  var now = new Date();
+  var timeDiff = date.getTime() - now.getTime();
+  if (timeDiff <= 0){
+    clearTimeout(timer);
+    document.write('Today is the Event!');
+  }
+  var seconds = Math.floor(timeDiff / 1000);
+  var minutes = Math.floor(seconds / 60);
+  var hours = Math.floor(minutes / 60);
+  var days = Math.floor(hours / 24);
+  var weeks = Math.floor(days / 7);
+  var months = Math.floor(weeks/ 4);
+  var years = Math.floor(weeks/ 12);
+  years %= 12;
+  weeks %= 7;
+  days %= 31;
+  hours %= 24;
+  minutes %= 60;
+  seconds %= 60;
+  document.getElementById('yearsBox').innerHTML = years;
+  document.getElementById('monthsBox').innerHTML = months;
+  document.getElementById('daysBox').innerHTML = days;
+  document.getElementById('hoursBox').innerHTML = hours;
+  document.getElementById('minutesBox').innerHTML = minutes;
+  document.getElementById('secondsBox').innerHTML = seconds;
+}
+cdtd();
+setInterval(cdtd,1000);
